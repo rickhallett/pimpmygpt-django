05736da3461842b6b2f336c61c0ba03a9d5e508d
Rick Hallett <rick.hallett@brandwatch.com>
2023-11-10 18:04:39 +0000

    go remote

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.CharField(max_length=500)
+    created_at = models.DateTimeField("date published")
+    
